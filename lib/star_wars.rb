1c655ab27eecb5a6fef7c711f9ea2e1e959cab60
Kayla Reopelle <kreopelle@gmail.com>
2018-06-19 22:21:39 +0000

    Initial commit

---
+++ lib/star_wars.rb
@@ -0,0 +1,10 @@
+require "star_wars/version"
+
+module StarWars
+  # Your code goes here...
+end
+
+require 'json'
+require 'rest-client'
+
+require_relative "star_wars/version"
---
+++ lib/star_wars/cli.rb
@@ -0,0 +1,7 @@
+class StarWars::CLI
+
+  def start
+    puts "Welcome to the Star Wars CLI!!"
+  end
+
+end
